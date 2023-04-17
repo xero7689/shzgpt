b63ace0c6b4415fac33ffd24e78ae1fffd0c9b04
xero7689 <volleyp7689@gmail.com>
2023-04-17 09:56:29 +0800

    feat: Add Theme Provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { useEffect, useState, useRef } from 'react';
+
+
+import { ThemeProvider } from '@mui/material/styles';
 import { Box, Container } from '@mui/material';
 
 import useInputControl from './control/inputControl';
@@ -9,6 +12,8 @@ import GPTSidePanel from './components/gptSidePanel';
 import MessageBox from './components/MessageBox';
 import InputForm from './components/InputForm';
 import LoadingBox from './components/LoadingBox';
+
+import myTheme from './theme';
 
 function App() {
   const systemMessage = "You're a helpful assistance.";
@@ -35,44 +40,46 @@ function App() {
 
   return (
     <div className="App" style={{ height: "100%" }}>
-      <Box display="flex" height="100%" flexDirection="column" >
-        <GPTAppBar ref={appBarRef} setToggleSidePanel={setToggleSidePanel}></GPTAppBar>
-        <Box display="flex" height="100%"
-          flexDirection={{
-            xs: "column",
-            md: "row"
-          }}
-          position={{
-            md: "static"
-          }}
-          sx={{
-            backgroundColor: '#1f2129',
-          }}
-        >
-          <GPTSidePanel setChatHistory={setChatHistory} setCurrentChatRoom={setCurrentChatRoom} toggleSidePanel={toggleSidePanel} />
-          <Container maxWidth="md">
-            <Box ref={chatInterfaceRef} flexGrow={1} display="flex" flexDirection="column" sx={{ height: chatInterfaceHeight, marginTop: "64px" }}>
-              <Box ref={chatContentRef} className="ChatContent" flexGrow={1} display="flex" flexDirection="column" gap={4} pt={4} py={3}
-                sx={{
-                  overflow: "auto",
-                  '&::-webkit-scrollbar': {
-                    display: 'none'
-                  },
-                }}
-              >
-                {chatHistory.map(item => {
-                  return (
-                    <MessageBox key={item.timestamp} timestamp={item.timestamp} role={item.role} content={item.content}>
-                    </MessageBox>
-                  )
-                })}
-                <LoadingBox queryInProgress={queryInProgress} />
+      <ThemeProvider theme={myTheme}>
+        <Box display="flex" height="100%" flexDirection="column" >
+          <GPTAppBar ref={appBarRef} setToggleSidePanel={setToggleSidePanel}></GPTAppBar>
+          <Box display="flex" height="100%"
+            flexDirection={{
+              xs: "column",
+              md: "row"
+            }}
+            position={{
+              md: "static"
+            }}
+            sx={{
+              backgroundColor: '#1f2129',
+            }}
+          >
+            <GPTSidePanel setChatHistory={setChatHistory} setCurrentChatRoom={setCurrentChatRoom} toggleSidePanel={toggleSidePanel} />
+            <Container maxWidth="md">
+              <Box ref={chatInterfaceRef} flexGrow={1} display="flex" flexDirection="column" sx={{ height: chatInterfaceHeight, marginTop: "64px" }}>
+                <Box ref={chatContentRef} className="ChatContent" flexGrow={1} display="flex" flexDirection="column" gap={4} pt={4} py={3}
+                  sx={{
+                    overflow: "auto",
+                    '&::-webkit-scrollbar': {
+                      display: 'none'
+                    },
+                  }}
+                >
+                  {chatHistory.map(item => {
+                    return (
+                      <MessageBox key={item.timestamp} timestamp={item.timestamp} role={item.role} content={item.content}>
+                      </MessageBox>
+                    )
+                  })}
+                  <LoadingBox queryInProgress={queryInProgress} />
+                </Box>
+                <InputForm {...inputFormProps} />
               </Box>
-              <InputForm {...inputFormProps} />
-            </Box>
-          </Container>
+            </Container>
+          </Box>
         </Box>
-      </Box>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import { orange } from '@mui/material/colors';
+
+const myTheme = createTheme({
+});
+
+export default myTheme;
