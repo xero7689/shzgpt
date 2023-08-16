70814cfaaad813bd6a98b4b21efb501c089b6364
xero7689 <volleyp7689@gmail.com>
2023-08-16 17:35:09 +0800

    feature: Add testing logger middleware

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { Middleware, MiddlewareAPI, Dispatch, Action } from "@reduxjs/toolkit";
+
 import chatRoomReducer from "../features/chatRoomSlice";
 import promptsReducer from "../features/promptsSlice";
 import promptTopicReducer from "../features/promptTopicSlice";
@@ -7,17 +9,27 @@ import settingsReducer from "../features/settingsSlice";
 import chatUserReducer from "../features/chatUserSlice";
 import apiKeySlice from "../features/apiKeySlice";
 
+const loggerMiddleware: Middleware = (store: MiddlewareAPI) => (next: Dispatch) => (action: Action) => {
+  console.group(action.type);
+  console.log("dispatching", action);
+  let result = next(action);
+  console.log("next state", store.getState());
+  console.groupEnd();
+  return result;
+};
+
 export const store = configureStore({
-    reducer: {
-        // This tells Redux that we want our top-level state object to have a field named posts inside,
-        // and all the data for state.posts will be updated by the postsReducer function when actions are dispatched.
-        chatRooms: chatRoomReducer,
-        prompts: promptsReducer,
-        promptTopic: promptTopicReducer,
-        settings: settingsReducer,
-        chatUser: chatUserReducer,
-        apiKey: apiKeySlice,
-    }
+  reducer: {
+    // This tells Redux that we want our top-level state object to have a field named posts inside,
+    // and all the data for state.posts will be updated by the postsReducer function when actions are dispatched.
+    chatRooms: chatRoomReducer,
+    prompts: promptsReducer,
+    promptTopic: promptTopicReducer,
+    settings: settingsReducer,
+    chatUser: chatUserReducer,
+    apiKey: apiKeySlice,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(loggerMiddleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
