2645ed04183551932e46f38eb29e79e788ab8dde
xero7689 <volleyp7689@gmail.com>
2023-10-29 20:40:37 +0800

    Update: Fix async-await issue in the onQueryStarted() function before executing queryFn()

--- src/lib/socketHelpers.ts
+++ src/lib/socketHelpers.ts
@@ -4,30 +4,26 @@ if (!envBaseUrl) {
   envBaseUrl = "ws://localhost:8000";
 }
 
-interface ShzGPTWebSocketConnectinos {
-  [key: string]: WebSocket;
+interface ShzGPTWebSocketItem {
+  socket: WebSocket;
+  onMessageEventListener: boolean;
+}
+
+interface ShzGPTWebSocketConnections {
+  [key: string]: ShzGPTWebSocketItem;
 }
 
 class WebSocketManager {
   baseUrl: string;
-  connections: ShzGPTWebSocketConnectinos;
+  connections: ShzGPTWebSocketConnections;
 
   constructor(baseUrl: string) {
     this.baseUrl = baseUrl;
     this.connections = {};
   }
 
-  list() {
-    console.log(this.connections);
-  }
-
   connect(urlPath: string): Promise<WebSocket> {
-    console.log("[WebSocket Manager] connect() trigger..");
-
     const url = new URL(urlPath, this.baseUrl);
-    if (urlPath in this.connections) {
-      return Promise.resolve(this.getConnection(urlPath));
-    }
 
     return new Promise((resolve, reject) => {
       let socket: WebSocket;
@@ -35,36 +31,50 @@ class WebSocketManager {
       socket = new WebSocket(url.href);
 
       socket.addEventListener("open", (event) => {
-        this.connections[urlPath] = socket;
-        console.log("[WebSocket Manager] connection resolve");
-        resolve(socket);
+        this.connections[urlPath] = {
+          socket: socket,
+          onMessageEventListener: false,
+        };
       });
 
       socket.addEventListener("close", (event) => {
-        console.log("[Websocket Manager]: Get connection closed event");
         delete this.connections[urlPath];
         socket.close();
       });
 
       socket.addEventListener("error", (event) => {
-        console.log("[Websocket Manager]: ", event);
         reject(event);
       });
+      resolve(socket);
     });
   }
 
-  async getConnection(urlPath: string): Promise<WebSocket> {
-    console.log(this.connections);
-    let socket = this.connections[urlPath];
-    if (urlPath in this.connections) {
-      return this.connections[urlPath];
-    } else {
-      return this.connect(urlPath);
+  async getAndReConnect(urlPath: string): Promise<ShzGPTWebSocketItem> {
+    if (!(urlPath in this.connections)) {
+      let socket = await this.connect(urlPath);
+      await new Promise((resolve) => {
+        const interval = setInterval(() => {
+          if (socket.readyState === WebSocket.OPEN) {
+            clearInterval(interval);
+            resolve(socket);
+          } else {
+          }
+        }, 500);
+      });
     }
+    return this.connections[urlPath];
+  }
+
+  getConnection(urlPath: string) {
+    return this.connections[urlPath];
+  }
+
+  hasConnection(urlPath: string) {
+    return this.connections[urlPath] !== undefined;
   }
 
   disconnect(urlPath: string) {
-    const socket = this.connections[urlPath];
+    const socket = this.connections[urlPath].socket;
     if (socket) {
       socket.close();
       delete this.connections[urlPath];
@@ -72,23 +82,18 @@ class WebSocketManager {
   }
 
   disconnectAll() {
-    Object.values(this.connections).forEach((socket) => {
-      socket.close();
+    Object.values(this.connections).forEach((item) => {
+      item.socket.close();
     });
 
     this.connections = {};
   }
 
   async safeSend(urlPath: string, request: Uint8Array) {
-    console.log("[WebSocket Manager][Safe Send]");
-    let socket = this.connections[urlPath];
-
-    if (socket.readyState == 3) {
-      console.log("Socket state is not OPEN, trying reconnect");
-      delete this.connections[urlPath];
-      socket = await this.connect(urlPath);
-    }
-    socket.send(request);
+    let socket = this.connections[urlPath].socket;
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.send(request);
+    } 
   }
 }
 
